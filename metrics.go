95c3a4e45f4534828465615b193efec5775177ad
Ananth Bhaskararaman <ananth.b@qubecinema.com>
2023-12-24 23:08:37 +0530

    Tidying up little things

--- metrics.go
+++ metrics.go
@@ -41,29 +41,28 @@ type hostMetrics struct {
 func newHostMetrics(host string) *hostMetrics {
 	return &hostMetrics{
 		requestsTotal: StatsForNerds.NewCounter(
-			fmt.Sprintf("chonker_http_requests_total{host=\"%s\"}", host),
+			fmt.Sprintf(`chonker_http_requests_total{host="%s"}`, host),
 		),
 		requestDurationSeconds: StatsForNerds.NewHistogram(
-			fmt.Sprintf("chonker_http_request_duration_seconds{host=\"%s\"}", host),
+			fmt.Sprintf(`chonker_http_request_duration_seconds{host="%s"}`, host),
 		),
 		requestSizeBytes: StatsForNerds.NewHistogram(
-			fmt.Sprintf("chonker_http_request_size_bytes{host=\"%s\"}", host),
+			fmt.Sprintf(`chonker_http_request_size_bytes{host="%s"}`, host),
 		),
 		requestChunkDurationSeconds: StatsForNerds.NewHistogram(
-			fmt.Sprintf("chonker_http_request_chunk_duration_seconds{host=\"%s\"}", host),
+			fmt.Sprintf(`chonker_http_request_chunk_duration_seconds{host="%s"}`, host),
 		),
 		requestChunkSizeBytes: StatsForNerds.NewHistogram(
-			fmt.Sprintf("chonker_http_request_chunk_size_bytes{host=\"%s\"}", host),
+			fmt.Sprintf(`chonker_http_request_chunk_size_bytes{host="%s"}`, host),
 		),
 	}
 }
 
 func getOrCreateHostMetrics(host string) *hostMetrics {
-	v, ok := hostMetricsMap.Load(host)
-	if ok {
-		return v.(*hostMetrics)
+	m, ok := hostMetricsMap.Load(host)
+	if !ok {
+		m = newHostMetrics(host)
+		hostMetricsMap.Store(host, m)
 	}
-	m := newHostMetrics(host)
-	hostMetricsMap.Store(host, m)
-	return m
+	return m.(*hostMetrics)
 }
