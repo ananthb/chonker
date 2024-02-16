712f605496acaf3726f62baa774bbc65d0814200
Ananth Bhaskararaman <ananth.b@qubecinema.com>
2024-02-16 22:13:05 +0530

    feat: Add coverage and clean up integration tests

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/ananthb/chonker
 
-go 1.21
+go 1.22
+
+toolchain go1.22.0
 
 require (
 	github.com/VictoriaMetrics/metrics v1.29.1
--- test.sh
+++ test.sh
@@ -6,15 +6,10 @@
 
 set -euo pipefail
 
+chonk="./bin/chonk_test"
 
-chonk=${CHONK:-}
-if [[ -z $chonk ]]; then
-	chonk="$(command -v chonk || true)"
-fi
-if [[ -z $chonk ]]; then
-	printf 'chonk not found\nSet CHONK environment variable to the path of chonk binary\n' >&2
-	exit 1
-fi
+mkdir -p bin
+go build -cover -o "$chonk" ./cmd/chonk
 
 r2_bucket_url="https://chonker-test.kedi.dev"
 download_urls=()
@@ -22,6 +17,10 @@ for file in "1_MiB.bin" "10_MiB.bin" "100MiB.bin"; do
 	download_urls+=("$r2_bucket_url/$file")
 done
 
+
 printf 'Testing chonk with %s URLs\n\n' "${#download_urls[@]}"
 
+rm -rf coverage
+mkdir -p coverage
+export GOCOVERDIR="coverage"
 xargs -n 1 "$chonk" -c 16MiB -o /dev/null < <(printf '%s\n' "${download_urls[@]}")
